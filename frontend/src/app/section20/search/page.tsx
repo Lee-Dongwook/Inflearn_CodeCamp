2d1ca7c33416156e4d131d4c7cd48bcabd2db1d0
Lee-Dongwook <dlehddnr0713@gmail.com>
2024-01-20 19:45:59 +0900

    feat : Search With Debouncing

---
+++ frontend/src/app/section20/search/page.tsx
@@ -0,0 +1,68 @@
+import React, { useState, type ChangeEvent, type MouseEvent } from "react";
+import { useQuery, gql } from "@apollo/client";
+
+interface Board {
+  _id: number;
+  writer: string;
+  title: string;
+  contents: string;
+}
+
+interface FetchBoardData {
+  fetchBoard: Board[];
+}
+
+const FETCH_BOARD = gql`
+  query fetchBoards($page: Int!, $search: String!) {
+    fetchBoard(page: $page, search: $search) {
+      _id
+      writer
+      title
+      contents
+    }
+  }
+`;
+
+export default function PaginationWithSearchPage() {
+  const [currentPage, setCurrentPage] = useState(1);
+  const [search, setSearch] = useState("");
+
+  const { data, refetch } = useQuery<FetchBoardData>(FETCH_BOARD, {
+    variables: { page: currentPage },
+  });
+
+  const onChangeSearch = (event: ChangeEvent<HTMLInputElement>): void => {
+    setSearch(event.target.value);
+  };
+
+  const onClickPage = (event: React.MouseEvent<HTMLSpanElement>): void => {
+    const clickedPage = Number(event.currentTarget.id);
+    setCurrentPage(clickedPage);
+    void refetch({ page: clickedPage });
+  };
+
+  const onClickSearch = (): void => {
+    void refetch({ search, page: 1 });
+  };
+
+  return (
+    <div>
+      검색어 입력: <input type="text" onChange={onChangeSearch} />
+      <button onClick={onClickSearch}>검색하기</button>
+      {data?.fetchBoard.map((el) => (
+        <div key={el._id} id={el.writer}>
+          <span>
+            <input type="checkbox" />
+          </span>
+          <span>{el.title}</span>
+          <span>{el.writer}</span>
+        </div>
+      ))}
+      {new Array(10).fill("").map((_, idx) => (
+        <span key={idx + 1} id={String(idx + 1)} onClick={onClickPage}>
+          {idx + 1}
+        </span>
+      ))}
+    </div>
+  );
+}
