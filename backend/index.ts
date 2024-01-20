15c626f3527b202dc7001b353134f7c898f1d195
Lee-Dongwook <dlehddnr0713@gmail.com>
2024-01-20 12:27:16 +0900

    feat : TypeORM을 사용한 Postgres 연동

--- backend/index.ts
+++ backend/index.ts
@@ -1,2 +1,20 @@
-const test: string = "Node.js 테스트";
-console.log(test);
+import { DataSource } from "typeorm";
+import { Board } from './Board.postgres';
+
+const AppDataSource = new DataSource({
+    type: "postgres",
+    host: "localhost",
+    port: 5432,
+    username: "postgres",
+    password: "1234",
+    database: "codeCamp",
+    entities: [Board],
+    synchronize: true,
+})
+
+
+AppDataSource.initialize().then(() => {
+    console.log("DB접속에 성공했습니다.");
+}).catch((error) => {
+    console.log("DB 접속에 실패하였습니다.", error);
+}) 
