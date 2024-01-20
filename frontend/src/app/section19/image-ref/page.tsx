698fb6e210349526ba0402b3d89f930d4769569b
Lee-Dongwook <dlehddnr0713@gmail.com>
2024-01-20 18:54:20 +0900

    feat: useRef을 이용한 파일 선택 부분 참조

---
+++ frontend/src/app/section19/image-ref/page.tsx
@@ -0,0 +1,50 @@
+import Image from "next/image";
+import { useState, useRef, type ChangeEvent } from "react";
+import { useMutation, gql } from "@apollo/client";
+
+const UPLOAD_FILE = gql`
+  mutation uploadFile($file: Upload!) {
+    uploadFile(file: $file) {
+      url
+    }
+  }
+`;
+
+export default function ImageRefPage() {
+  const [uploadFile] = useMutation(UPLOAD_FILE);
+  const fileRef = useRef<HTMLInputElement>(null);
+  const [imageUrl, setImageUrl] = useState<string>("");
+
+  const onChangeFile = async (
+    event: ChangeEvent<HTMLInputElement>
+  ): Promise<void> => {
+    const file = event.target.files?.[0];
+    console.log(file);
+
+    const result = await uploadFile({ variables: { file } });
+    console.log(result.data?.uploadFile.url);
+    setImageUrl(result.data?.uploadFile.url ?? "");
+  };
+
+  const onClickImage = () => {
+    fileRef.current?.click();
+  };
+
+  return (
+    <>
+      <div
+        style={{ width: "50px", height: "50px", backgroundColor: "gray" }}
+        onClick={onClickImage}
+      >
+        이미지 선택
+      </div>
+      <input
+        style={{ display: "none" }}
+        type="file"
+        onChange={onChangeFile}
+        ref={fileRef}
+      />
+      <Image src={`https://storage.googleapis.com/${imageUrl}`} alt="사진" />
+    </>
+  );
+}
